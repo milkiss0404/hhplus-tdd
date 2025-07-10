3a14f5181a8ec0e2a17931ac14e1ffdd5c4faa54
i love milkiss <92104561+milkiss0404@users.noreply.github.com>
2025-07-11 01:48:45 +0900

    포인트 충전시 히스토리 합계 계산후 유저 포인트에 입력 구현

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,4 +1,30 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class PointService {
+    private final PointHistoryTable pointHistoryTable;
+    private final PointChecking pointChecking;
+    private final UserPointTable userPointTable;
+
+    public UserPoint chargeUserPoint(Long id, Long amount) throws Exception {
+        pointChecking.checkBalance(id, amount, TransactionType.CHARGE);
+
+        pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        return sumAndInsertPoint(id);
+    }
+
+    private UserPoint sumAndInsertPoint(Long id) {
+        List<PointHistory> pointHistories = pointHistoryTable.selectAllByUserId(id);
+        long sum = pointHistories.stream().mapToLong(PointHistory::amount).sum();
+        return userPointTable.insertOrUpdate(id, sum);
+    }
 }
