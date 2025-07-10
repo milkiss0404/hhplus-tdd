88a9c0ea5b45ae47aa5b4a3ab23ea0e2c07a3489
i love milkiss <92104561+milkiss0404@users.noreply.github.com>
2025-07-11 01:43:24 +0900

    포인트 잔액 조회 구현 & GREEN

--- src/main/java/io/hhplus/tdd/point/PointChecking.java
+++ src/main/java/io/hhplus/tdd/point/PointChecking.java
@@ -1,4 +1,23 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
 public class PointChecking {
+    private final PointHistoryTable pointHistoryTable;
+    private final UserPointTable userPointTable;
+    private final Long MAX_POINT = 1_000_000L;
+
+
+    public UserPoint getBalance(Long userId) {
+        List<PointHistory> pointHistories = pointHistoryTable.selectAllByUserId(userId);
+        long sum = pointHistories.stream().mapToLong(PointHistory::amount).sum();
+        return userPointTable.insertOrUpdate(userId, sum);
+    }
 }
